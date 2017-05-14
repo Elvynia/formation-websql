3227d81628887e75e639fc88be3793058bab5251
Elvynia <elvynia@gmail.com>
2017-05-14 14:30:36 +0200

    Ajout de l'implémentation de Serializable car les articles transitent dans les attributs des requêtes HTTP, et donc sont transformées en flux.

--- src/main/java/fr/formation/websql/entity/Article.java
+++ src/main/java/fr/formation/websql/entity/Article.java
@@ -1,6 +1,10 @@
 package fr.formation.websql.entity;
 
-public class Article {
+import java.io.Serializable;
+
+public class Article implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 
